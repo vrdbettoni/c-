84bc03061f2703b0a9b9c02a83b745d9161f8e9c
vrdbettoni <vroth-di@student.42lyon.fr>
2021-02-01 13:41:31 +0100

    change cout

--- 03/ex00/FragTrap.cpp
+++ 03/ex00/FragTrap.cpp
@@ -2,21 +2,21 @@
 
 FragTrap::FragTrap()
 {
-    std::cout << "Default constructor called" << std::endl;
-    std::cout << "His name is Bobby" << std::endl;
+    std::cout << "FR4G-TP " << "Default constructor called" << std::endl;
+    std::cout << "FR4G-TP " << "His name is Bobby" << std::endl;
     _name = "Bobby";
     FragTrap::setStats();
 }
 
 FragTrap::FragTrap(std::string name) : _name(name)
 {
-    std::cout << "FragTrap " << _name << " created" << std::endl;
+    std::cout << "FR4G-TP " << "FragTrap " << _name << " created" << std::endl;
     FragTrap::setStats();
 }
 
 FragTrap::~FragTrap()
 {
-    std::cout << _name << " deleted" << std::endl;
+    std::cout << "FR4G-TP " << _name << " deleted" << std::endl;
 }
 
 void FragTrap::setStats()
@@ -49,10 +49,10 @@ void FragTrap::takeDamage(unsigned int amount)
         if (_amount > _hitPoints)
             _amount = _hitPoints;
         _hitPoints -= _amount;
-        std::cout << _name << " loses " << _amount << " hit points (" << _hitPoints << " left)" << std::endl;
+        std::cout << "FR4G-TP " << _name << " loses " << _amount << " hit points (" << _hitPoints << " left)" << std::endl;
     }
     else
-        std::cout << _name << " is so strong" << std::endl;
+        std::cout << "FR4G-TP " << _name << " is so strong" << std::endl;
 }
 
 void FragTrap::beRepaired(unsigned int amount)
@@ -64,16 +64,16 @@ void FragTrap::beRepaired(unsigned int amount)
         _hitPoints += _amount;
     if (_hitPoints > _maxHitPoints)
         _hitPoints = _maxHitPoints;
-     std::cout << _name << " gained " << _amount << " hit points (" << _hitPoints << " left)" << std::endl;
+     std::cout << "FR4G-TP " << _name << " gained " << _amount << " hit points (" << _hitPoints << " left)" << std::endl;
 }
 
 void FragTrap::vaulthunter_dot_exe(std::string const &target)
 {
     std::string attack[5] = {" with an m&m's", " with an egg", " with nothing", " with a knife", " with a saber"};
     if (_energyPoints >= 25){
-        std::cout << _name << " attack " << target << attack[(rand()%50) / 10] << std::endl;
+        std::cout << "FR4G-TP " << _name << " attack " << target << attack[(rand()%50) / 10] << std::endl;
         _energyPoints -= 25;
     }
     else
-        std::cout << "not enough energy points" << std::endl;
+        std::cout << "FR4G-TP " << "not enough energy points" << std::endl;
 }
---
+++ 03/ex01/ScavTrap.hpp
@@ -0,0 +1,33 @@
+#ifndef SCAVTRAP_HPP
+# define SCAVTRAP_HPP
+
+#include <iostream>
+#include <cstdlib>
+
+class FragTrap
+{
+    public:
+        ScavTrap();
+        ScavTrap(std::string name);
+        ~ScavTrap();
+    
+        void rangedAttack(std::string const &target);
+        void meleeAttack(std::string const &target);
+        void takeDamage(unsigned int amount);
+        void beRepaired(unsigned int amount);
+        void vaulthunter_dot_exe(std::string const &target);
+
+    private:
+        int _hitPoints;
+        int _maxHitPoints;
+        int _energyPoints;
+        int _level;
+        int _meleeAttackDamage;
+        int _rangedAttackDamage;
+        int _armorReduction;
+
+        std::string _name;
+        void    setStats();
+};
+
+#endif
